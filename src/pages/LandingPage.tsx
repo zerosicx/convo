0de8e3101cd55c1226b56ef4c014e7afbe8932c7
zerosicx <hcsofteng@gmail.com>
2024-12-09 22:22:42 +1300

    Updated font

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,9 +1,11 @@
-
-
-
 const LandingPage = () => {
     return (
-        <div>LandingPage</div>
+        <div className="w-full h-screen flex flex-col items-center justify-center">
+            <div className="flex flex-row justify-between fixed top-0 h-16 items-center">
+                <div>Mizu</div>
+            </div>
+            <div>Main section</div>
+        </div>
     )
 }
 
