9d079235e4a69312a0d0fb9ddb5f376433f8c363
Hannah Carino <hcsofteng@gmail.com>
2024-12-15 01:56:40 +1300

    Employ HashRouter

--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Outlet, Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import { Outlet, Route, HashRouter as Router, Routes } from "react-router-dom";
 
 // Pages
 import ErrorPage from "@/pages/ErrorPage";
@@ -11,31 +11,31 @@ import MainLayout from "@/layouts/MainLayout";
 import AboutPage from "@/pages/AboutPage";
 
 const AppRouter: React.FC = () => {
-    return (
-        <Router>
-            <Routes>
-                {/* Landing Page */}
-                <Route path="/" element={<LandingPage />} />
+  return (
+    <Router>
+      <Routes>
+        {/* Landing Page */}
+        <Route path="/" element={<LandingPage />} />
 
-                {/* About page */}
-                <Route path="/about" element={<AboutPage />} />
+        {/* About page */}
+        <Route path="/about" element={<AboutPage />} />
 
-                {/* Application Dashboard */}
-                <Route path="/app" element={<MainLayout />}>
-                    <Route index element={<Outlet />} />
-                    <Route path="notebook/:notebookId" element={<Outlet />} >
-                        <Route path="section/:sectionId" element={<Outlet />}>
-                            <Route path="page/:pageId" element={<Outlet />} />
-                        </Route>
-                    </Route>
-                </Route>
+        {/* Application Dashboard */}
+        <Route path="/app" element={<MainLayout />}>
+          <Route index element={<Outlet />} />
+          <Route path="notebook/:notebookId" element={<Outlet />}>
+            <Route path="section/:sectionId" element={<Outlet />}>
+              <Route path="page/:pageId" element={<Outlet />} />
+            </Route>
+          </Route>
+        </Route>
 
-                {/* Catch-All Route for 404 */}
-                <Route path="*" element={<ErrorPage />} />
-            </Routes>
-            <Toaster />
-        </Router>
-    );
+        {/* Catch-All Route for 404 */}
+        <Route path="*" element={<ErrorPage />} />
+      </Routes>
+      <Toaster />
+    </Router>
+  );
 };
 
 export default AppRouter;
